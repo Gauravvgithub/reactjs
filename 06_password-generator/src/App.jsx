15d97745c2de007a6b37e2a53c319c624ce533b9
Gaurav <gauravraj7011@gmail.com>
2025-03-14 22:54:19 +0530

    password generator updates

--- 06_password-generator/src/App.jsx
+++ 06_password-generator/src/App.jsx
@@ -38,8 +38,8 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-8 py-4 my-50 bg-gray-800 text-orange-500">
-        <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-8 py-4 my-50 bg-gray-800 text-amber-400">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
